5603e36b7e65369ebb6d5f3527e8f18cdc774b32
Alawiye Muritala <naijabayz@gmail.com>
2025-08-11 15:22:55 +0100

    fix: image not displaying on cloud host

--- data.js
+++ data.js
@@ -4,7 +4,7 @@ const data =  {
              {
                 name: "Relate+ CRM",
                 desc : 'A mini Customer Relationship Management Web App',
-                imageURL : "./src/assets/images/web (1).png",
+                imageURL : "/src/assets/images/web (1).png",
                 stack : "VUE.js | Firebase",
                 link : "http://relateplus.netlify.app",
                 githubLink : "http://github.com/emdes12/relateplus.git"
@@ -12,7 +12,7 @@ const data =  {
             {
                 name : "Ribdex Property",
                 desc : 'A website build for Ribdex Global Concept Ltd for listing their available property',
-                imageURL : "./src/assets/images/web (4).png",
+                imageURL : "/src/assets/images/web (4).png",
                 stack : "HTML | Tailwind CSS | JS | Firebase",
                 link : "http://new-ribdex-property.netlify.app",
                 githubLink : "http://github.com/emdes12/ribdex-property.git"
@@ -20,7 +20,7 @@ const data =  {
             {
                 name : "PayEvi",
                 desc : 'A Online Invoice Generator Web App',
-                imageURL : "./src/assets/images/web (3).png",
+                imageURL : "/src/assets/images/web (3).png",
                 stack : "VUE.js | Firebase",
                 link : "http://payevi.vercel.app",
                 githubLink : "http://github.com/emdes12/payevi.git"
@@ -28,7 +28,7 @@ const data =  {
             {
                 name : "Ribdex Global Concept Website",
                 desc : 'Ribdex Global Concept Limited Website',
-                imageURL : "./src/assets/images/web (2).png",
+                imageURL : "/src/assets/images/web (2).png",
                 stack : "HTML | Tailwind CSS | JS",
                 link : "http://ribdexglobal.netlify.app",
                 githubLink : "http://github.com/emdes12/Ribdex-Real-Repo.git"
@@ -38,53 +38,53 @@ const data =  {
             {
                 name : "Relate+ CRM",
                 desc : 'A mini Customer Relationship Management Web App',
-                imageURL : "./src/assets/images/logo (1).jpg",
+                imageURL : "/src/assets/images/logo (1).jpg",
                 stack : "CorelDraw",
-                link : "http://alawiye.netlify.app./src/assets/images/logo (1).jpg",
+                link : "http://alawiye.netlify.app/src/assets/images/logo (1).jpg",
             },
             {
                 name : "Havan FX",
                 desc : 'A Forex Trading Company into Investment and Training',
-                imageURL : "./src/assets/images/logo (4).jpg",
+                imageURL : "/src/assets/images/logo (4).jpg",
                 stack : "CorelDraw",
-                link : "http://alawiye.netlify.app./src/assets/images/logo (4).jpg",
+                link : "http://alawiye.netlify.app/src/assets/images/logo (4).jpg",
             },
             {
                 name : "Osun Aseyori",
                 desc : 'A Tradomedical Center',
-                imageURL : "./src/assets/images/logo (3).jpg",
+                imageURL : "/src/assets/images/logo (3).jpg",
                 stack : "CorelDraw",
-                link : "http://alawiye.netlify.app./src/assets/images/logo (3).jpg",
+                link : "http://alawiye.netlify.app/src/assets/images/logo (3).jpg",
             },
             {
                 name : "Luke Ink Notary",
                 desc : 'a mini Customer Relationship Management Web App',
-                imageURL : "./src/assets/images/logo (2).jpg",
+                imageURL : "/src/assets/images/logo (2).jpg",
                 stack : "CorelDraw",
-                link : "http://alawiye.netlify.app./src/assets/images/logo (2).jpg",
+                link : "http://alawiye.netlify.app/src/assets/images/logo (2).jpg",
             },
         ],
         flyer : [
             {
                 name : "Al-Itqon Online Academy",
                 desc : 'An online institue for learning Tajweed and Memorizing the Quran',
-                imageURL : "./src/assets/images/flyer (1).png",
+                imageURL : "/src/assets/images/flyer (1).png",
                 stack : "CorelDraw",
-                link : "./src/assets/images/flyer (1).png",
+                link : "/src/assets/images/flyer (1).png",
             },
             {
                 name : "Siaat Cakes Training",
                 desc : 'Ballon Class Training organized by Siaat Cakes and Party Planning',
-                imageURL : "./src/assets/images/flyer (2).jpg",
+                imageURL : "/src/assets/images/flyer (2).jpg",
                 stack : "CorelDraw",
-                link : "./src/assets/images/flyer (2).jpg",
+                link : "/src/assets/images/flyer (2).jpg",
             },
             {
                 name : "Ribdex Property",
                 desc : 'A property showcasing flyer',
-                imageURL : "./src/assets/images/flyer (3).jpg",
+                imageURL : "/src/assets/images/flyer (3).jpg",
                 stack : "CorelDraw",
-                link : "./src/assets/images/flyer (3).jpg",
+                link : "/src/assets/images/flyer (3).jpg",
             },
         ],
     }
